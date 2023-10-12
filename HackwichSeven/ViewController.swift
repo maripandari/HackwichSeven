a6d6abf082e21ee3974b6972410e426fc0cfdec4
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-10-12 13:32:07 -1000

    Initial Commit

---
+++ HackwichSeven/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackwichSeven
+//
+//  Created by Marijoy Jatico on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
